7d9085a5ae8fb28557c248435fd8e65a652b3dc7
David Anthoff <anthoff@berkeley.edu>
2016-09-21 09:50:08 -0700

    Make it work with julia 0.5

--- scripts/lintserver/lintserver.jl
+++ scripts/lintserver/lintserver.jl
@@ -1,5 +1,5 @@
-if VERSION < v"0.4" || VERSION >= v"0.5-"
-    println("VS Code linter only works with julia 0.4")
+if VERSION < v"0.4" || VERSION >= v"0.6-"
+    println("VS Code linter only works with julia 0.4 and 0.5")
 else
     lock_aquired = false
     while !lock_aquired
@@ -13,8 +13,8 @@ else
                 catch e
                     println("Installing Lint package")
                     Pkg.init()
-                    Pkg.add("Compat", v"0.8.4")
-                    Pkg.add("Lint", v"0.2.3")
+                    Pkg.add("Compat", v"0.9.2")
+                    Pkg.add("Lint", v"0.2.5")
                     eval(parse("using Lint"))
                 end
             finally
