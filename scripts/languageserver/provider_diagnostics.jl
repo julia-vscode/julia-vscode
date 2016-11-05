c118eb4b62e083a3e59cd5de30ba89fd9a044184
David Anthoff <anthoff@berkeley.edu>
2016-11-04 19:34:37 -0700

    Correctly compute start column for lint messages

--- scripts/languageserver/provider_diagnostics.jl
+++ scripts/languageserver/provider_diagnostics.jl
@@ -1,9 +1,11 @@
 const LintSeverity = Dict('E'=>1,'W'=>2,'I'=>3)
 
 function process_diagnostics(uri::String, server::LanguageServer)
-    L = lintfile(URI(replace(unescape(uri),"\\","/")).path[2:end],join(server.documents[uri],'\n'))
+    document = server.documents[uri]
+    L = lintfile(URI(replace(unescape(uri),"\\","/")).path[2:end],join(document,'\n'))
     diags = map(L) do l
-        Diagnostic(Range(Position(l.line-1, 0), Position(l.line-1, typemax(Int)) ),
+        start_col = findfirst(i->i!=' ', document[l.line])-1
+        Diagnostic(Range(Position(l.line-1, start_col), Position(l.line-1, typemax(Int)) ),
                         LintSeverity[string(l.code)[1]],
                         string(l.code),
                         "Lint.jl",
