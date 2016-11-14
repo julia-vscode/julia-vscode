87c4cf5f984e2462e443c6c15a35637fcf071865
ZacNugent <zac.nugent@gmail.com>
2016-11-14 17:47:48 +0000

    Update languageserver.jl

--- scripts/languageserver/languageserver.jl
+++ scripts/languageserver/languageserver.jl
@@ -7,6 +7,7 @@ type LanguageServer
     DocStore::Dict{String,Any}
 
     function LanguageServer(pipe_in,pipe_out)
+        
         new(pipe_in,pipe_out,"",Dict{String,Array{UInt8,1}}(),Dict{String,Any}())
     end
 end
