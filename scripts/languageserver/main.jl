50d121aebaab8dae60def0831a492bc0121288fd
ZacLN <zac.nugent@gmail.com>
2017-05-01 19:12:28 +0000

    change used module

--- scripts/languageserver/main.jl
+++ scripts/languageserver/main.jl
@@ -1,27 +1,25 @@
-if VERSION < v"0.5"
-    error("VS Code julia language server only works with julia 0.5 or newer.")
+if VERSION < v"0.6.0-pre.beta.400"
+    error("VS Code julia language server only works with julia 0.6 or newer.")
 end
 
-if length(Base.ARGS)!=2
+if length(Base.ARGS) != 2
     error("Invalid number of arguments passed to julia language server.")
 end
 
 conn = STDOUT
 (outRead, outWrite) = redirect_stdout()
 
-if Base.ARGS[2]=="--debug=no"
+if Base.ARGS[2] == "--debug=no"
     const global ls_debug_mode = false
-elseif Base.ARGS[2]=="--debug=yes"
+elseif Base.ARGS[2] == "--debug=yes"
     const global ls_debug_mode = true
 end
 
-push!(LOAD_PATH, joinpath(dirname(@__FILE__),"packages"))
+push!(LOAD_PATH, joinpath(dirname(@__FILE__), "packages"))
 push!(LOAD_PATH, Base.ARGS[1])
 
 using Compat
-using JSON
-using URIParser
 using LanguageServer
 
-server = LanguageServerInstance(STDIN,conn, ls_debug_mode, Base.ARGS[1])
+server = LanguageServerInstance(STDIN, conn, ls_debug_mode, Base.ARGS[1])
 run(server)
