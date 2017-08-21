0792dac02861ec7d6df428c3a774edcc6d469fd4
ZacNugent <zac.nugent@gmail.com>
2017-08-21 22:06:58 +0000

    Update terminalserver.jl

--- scripts/terminalserver/terminalserver.jl
+++ scripts/terminalserver/terminalserver.jl
@@ -6,13 +6,50 @@ immutable InlineDisplay <: Display end
 
 pid = Base.ARGS[1]
 
+function change_module(newmodule::String)
+    smods = Symbol.(split(newmodule, "."))
+
+    val = Main
+    for i = 1:length(smods)
+        if isdefined(val, smods[i])
+            val = getfield(val, smods[i])
+        else
+            println("Could not find module $newmodule")
+            return
+        end
+    end
+    expr = parse(newmodule)
+    repl = Base.active_repl
+    main_mode = repl.interface.modes[1]
+    main_mode.on_done = Base.REPL.respond(repl,main_mode; pass_empty = false) do line
+        if !isempty(line)
+            :( eval($expr, parse($line)) )
+        else
+            :(  )
+        end
+    end
+    println("Changed root module to $expr")
+    print_with_color(:green, "julia> ", bold = true)
+end
+
 if is_windows()
     global_lock_socket_name = "\\\\.\\pipe\\vscode-language-julia-terminal-$pid"
+    modchange_sock_name = "\\\\.\\pipe\\vscode-language-julia-modchange-$pid"
 elseif is_unix() 
     global_lock_socket_name = joinpath(tempdir(), "vscode-language-julia-terminal-$pid")
+    modchange_sock_name = joinpath(tempdir(), "vscode-language-julia-modchange-$pid")
 else
     error("Unknown operating system.")
 end
+
+@async begin
+        server = listen(modchange_sock_name)
+        while true
+            sock = accept(server)
+            msg = readline(sock)
+            change_module(strip(msg, '\n'))
+        end
+    end
 
 conn = connect(global_lock_socket_name)
 
