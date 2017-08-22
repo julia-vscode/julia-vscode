dd77a05c89f065234d830e45ca7016b0dea04b7c
ZacLN <zac.nugent@gmail.com>
2017-08-22 08:50:50 +0000

    change prompt to module name

--- scripts/terminalserver/terminalserver.jl
+++ scripts/terminalserver/terminalserver.jl
@@ -21,6 +21,7 @@ function change_module(newmodule::String)
     expr = parse(newmodule)
     repl = Base.active_repl
     main_mode = repl.interface.modes[1]
+    main_mode.prompt = string(newmodule,">")
     main_mode.on_done = Base.REPL.respond(repl,main_mode; pass_empty = false) do line
         if !isempty(line)
             :( eval($expr, parse($line)) )
@@ -29,7 +30,7 @@ function change_module(newmodule::String)
         end
     end
     println("Changed root module to $expr")
-    print_with_color(:green, "julia> ", bold = true)
+    print_with_color(:green, string(newmodule,">"), bold = true)
 end
 
 function get_available_modules(m=Main)
