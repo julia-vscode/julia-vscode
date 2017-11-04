d0e3d05e7429311a284bfe8f1cc4e281b92119ec
David Anthoff <anthoff@berkeley.edu>
2017-11-03 17:21:50 -0700

    Fix focus issue related to REPL

--- src/repl.ts
+++ src/repl.ts
@@ -257,7 +257,6 @@ function executeSelection() {
         }
     }
     executeCode(text)
-    vscode.window.showTextDocument(editor.document);
 }
 
 function executeFile() {
@@ -278,14 +277,12 @@ function executeJuliaBlockInRepl() {
         g_languageClient.sendRequest('julia/getCurrentBlockOffsetRange', params).then((param) => {
             executeCode(vscode.window.activeTextEditor.document.getText(new vscode.Range(vscode.window.activeTextEditor.document.positionAt(param[0]), vscode.window.activeTextEditor.document.positionAt(param[1]))))
             vscode.window.activeTextEditor.selection = new vscode.Selection(vscode.window.activeTextEditor.document.positionAt(param[1]), vscode.window.activeTextEditor.document.positionAt(param[1]))
-            vscode.window.showTextDocument(editor.document);
         })
     }
 }
 
 function changeREPLmode() {
     sendMessage('repl/getAvailableModules', '');
-    vscode.window.showTextDocument(vscode.window.activeTextEditor.document);
 }
 
 function sendMessage(cmd, msg: string) {
