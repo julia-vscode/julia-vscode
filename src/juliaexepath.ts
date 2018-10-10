f484fe858e8dcf0ef468a958b53e3008278a18c5
David Anthoff <anthoff@berkeley.edu>
2018-10-09 18:12:52 -0700

    Search for julia 1.0

--- src/juliaexepath.ts
+++ src/juliaexepath.ts
@@ -19,15 +19,14 @@ export async function getJuliaExePath() {
             let pathsToSearch = [];
             if (process.platform == "win32") {
                 pathsToSearch = ["julia.exe",
-                    path.join(homedir, "AppData", "Local", "Julia-0.6.3", "bin", "julia.exe"),
-                    path.join(homedir, "AppData", "Local", "Julia-0.6.2", "bin", "julia.exe"),
-                    path.join(homedir, "AppData", "Local", "Julia-0.6.1", "bin", "julia.exe"),
-                    path.join(homedir, "AppData", "Local", "Julia-0.6.0", "bin", "julia.exe")];
+                    path.join(homedir, "AppData", "Local", "Julia-1.0.0", "bin", "julia.exe"),
+                    path.join(homedir, "AppData", "Local", "Julia-1.0.1", "bin", "julia.exe")
+                ];
             }
             else if (process.platform == "darwin") {
                 pathsToSearch = ["julia",
-                    path.join(homedir, "Applications", "Julia-0.6.app", "Contents", "Resources", "julia", "bin", "julia"),
-                    path.join("/", "Applications", "Julia-0.6.app", "Contents", "Resources", "julia", "bin", "julia")];
+                    path.join(homedir, "Applications", "Julia-1.0.app", "Contents", "Resources", "julia", "bin", "julia"),
+                    path.join("/", "Applications", "Julia-1.0.app", "Contents", "Resources", "julia", "bin", "julia")];
             }
             else {
                 pathsToSearch = ["julia"];
@@ -35,7 +34,7 @@ export async function getJuliaExePath() {
             let foundJulia = false;
             for (let p of pathsToSearch) {
                 try {
-                    var res = await exec(`"${p}" --startup-file=no --history-file=no -e "println(VERSION < v\\"0.7-\\" ? JULIA_HOME : Sys.BINDIR)"`);
+                    var res = await exec(`"${p}" --startup-file=no --history-file=no -e "println(Sys.BINDIR)"`);
                     if (p == 'julia' || p == "julia.exe") {
                         // use full path
                         actualJuliaExePath = path.join(res.stdout.trim(), p);
