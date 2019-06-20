8a2cecebfcd610998a33d5dbe60516f5b73622f4
Tracy Wadleigh <tracy@compoundeye.com>
2019-06-20 14:02:30 -0700

    Added markdown codeblock injection grammar.
    Also update_syntax.jl now can accept a grammar file on the command line.
    
    The injection grammar properly highlights julia code embedded in a markdown file
    but does not yet highlight julia code embedded in markdown embedded in julia.

--- syntaxes/update_syntax.jl
+++ syntaxes/update_syntax.jl
@@ -1,32 +1,34 @@
-# Converts the latest grammar from atom-language-julia to one compatible with vscode
+#! julia
+# Converts an atom-language-julia grammar into one compatible with vscode
 # Requires:
 #  - HTTP.jl to be installed;
 #  - `cson2json` to be on your path. (Install via npm.)
-
 import HTTP
 
-const url = "https://raw.githubusercontent.com/JuliaEditorSupport/atom-language-julia/master/grammars/julia.cson"
+url = "https://raw.githubusercontent.com/JuliaEditorSupport/atom-language-julia/master/grammars/julia.cson"
 
-HTTP.open("GET", url) do cson
-  open(pipeline(`cson2json`, stdin=IOBuffer(read(cson))), "r") do c2j
-    # get the raw json
-    json = read(c2j, String)
+# get cson - download if filename not given
+cson = isempty(ARGS) ? String(HTTP.get(url).body) : read(ARGS[1], String)
 
-    # apply substitutions
-    sub!(pr) = json = replace(json, pr; count=typemax(Int))
+# convert cson to json
+json = Ref(read(open(pipeline(`cson2json`, stdin=IOBuffer(cson))), String))
 
-    # when the juliamarkdown grammar is converted into an injection, this will also highlight embedded julia
-    sub!(r"(\"include\"\s*:\s*\")source\.gfm(\")" => s"\1text.html.markdown\2")
-    sub!(r"(\"include\"\s*:\s*\"source\.cpp)(\")" => s"\1#root_context\2")
-    sub!(r"(\"contentName\"\s*:\s*\")source\.cpp(\")" => s"\1meta.embedded.block.cpp\2")
-    sub!(r"(\"contentName\"\s*:\s*\")source\.gfm(\")" => s"\1meta.embedded.block.markdown\2")
-    sub!(r"(\"contentName\"\s*:\s*\")source\.js(\")" => s"\1meta.embedded.block.js\2")
-    sub!(r"(\"contentName\"\s*:\s*\")source\.r(\")" => s"\1meta.embedded.block.r\2")
-    sub!(r"(\"contentName\"\s*:\s*\")source\.python(\")" => s"\1meta.embedded.block.python\2")
+# apply substitutions
+sub!(pr) = json.x = replace(json.x, pr; count=typemax(Int))
 
-    # print out the transformed syntax
-    println(json)
-  end
-end
+sub!(r"(\"include\"\s*:\s*\")source\.gfm(\")" => s"\1text.html.markdown\2")
 
-##
+# Skip over-zealous top-level production in `source.cpp`. See offending pattern here:
+# https://github.com/microsoft/vscode/blob/c3fe2d8acde04e579880413ae4622a1f551efdcc/extensions/cpp/syntaxes/cpp.tmLanguage.json#L745
+sub!(r"(\"include\"\s*:\s*\"source\.cpp)(\")" => s"\1#root_context\2")
+
+# Choose content names consistent with the vscode conventions for embedded code. Cf.:
+# https://code.visualstudio.com/api/language-extensions/syntax-highlight-guide#embedded-languages
+sub!(r"(\"contentName\"\s*:\s*\")source\.cpp(\")" => s"\1meta.embedded.block.cpp\2")
+sub!(r"(\"contentName\"\s*:\s*\")source\.gfm(\")" => s"\1meta.embedded.block.markdown\2")
+sub!(r"(\"contentName\"\s*:\s*\")source\.js(\")" => s"\1meta.embedded.block.javascript\2")
+sub!(r"(\"contentName\"\s*:\s*\")source\.r(\")" => s"\1meta.embedded.block.r\2")
+sub!(r"(\"contentName\"\s*:\s*\")source\.python(\")" => s"\1meta.embedded.block.python\2")
+
+# print out the transformed syntax
+println(json.x)
