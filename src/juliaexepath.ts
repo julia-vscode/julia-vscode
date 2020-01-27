fa5f4dd0e58bb323f2aa9f3817b18a8658ad5b05
David Anthoff <anthoff@berkeley.edu>
2020-01-27 11:00:38 -0800

    Remove support for Julia 1.4

--- src/juliaexepath.ts
+++ src/juliaexepath.ts
@@ -38,7 +38,6 @@ export async function getJuliaExePath() {
             let pathsToSearch = [];
             if (process.platform == "win32") {
                 pathsToSearch = ["julia.exe",
-                    path.join(homedir, "AppData", "Local", "Julia-1.4.0", "bin", "julia.exe"),
                     path.join(homedir, "AppData", "Local", "Julia-1.3.2", "bin", "julia.exe"),
                     path.join(homedir, "AppData", "Local", "Julia-1.3.1", "bin", "julia.exe"),
                     path.join(homedir, "AppData", "Local", "Julia-1.3.0", "bin", "julia.exe"),
@@ -56,8 +55,6 @@ export async function getJuliaExePath() {
             }
             else if (process.platform == "darwin") {
                 pathsToSearch = ["julia",
-                    path.join(homedir, "Applications", "Julia-1.4.app", "Contents", "Resources", "julia", "bin", "julia"),
-                    path.join("/", "Applications", "Julia-1.4.app", "Contents", "Resources", "julia", "bin", "julia"),
                     path.join(homedir, "Applications", "Julia-1.3.app", "Contents", "Resources", "julia", "bin", "julia"),
                     path.join("/", "Applications", "Julia-1.3.app", "Contents", "Resources", "julia", "bin", "julia"),
                     path.join(homedir, "Applications", "Julia-1.2.app", "Contents", "Resources", "julia", "bin", "julia"),
