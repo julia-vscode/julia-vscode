d39b67ada701befcfa5a89b7ef1039a8bddeed7e
David Anthoff <anthoff@berkeley.edu>
2020-02-09 12:48:12 -0800

    Use our own copy of JuliaInterpreter

--- scripts/debugger/debugger.jl
+++ scripts/debugger/debugger.jl
@@ -1,6 +1,18 @@
 module VSCodeDebugger
 
-import JuliaInterpreter
+include("../terminalserver/repl.jl")
+
+# This patches JuliaInterpreter.jl to use our private copy of CodeTracking.jl
+filename_of_juliainterpreter = joinpath(@__DIR__, "packages", "JuliaInterpreter", "src", "JuliaInterpreter.jl")
+filename_of_codetracking = joinpath(@__DIR__, "packages", "CodeTracking", "src", "CodeTracking.jl")
+filename_of_codetracking = replace(filename_of_codetracking, "\\"=>"\\\\")
+jlinterp_code = read(filename_of_juliainterpreter, String)
+jlinterp_code_patched = replace(jlinterp_code, "using CodeTracking"=>"include(\"$filename_of_codetracking\"); using .CodeTracking")
+withpath(filename_of_juliainterpreter) do
+    include_string(VSCodeDebugger, jlinterp_code_patched, filename_of_juliainterpreter)
+end
+
+import .JuliaInterpreter
 import Sockets, Base64
 
 @inline function _parse_julia_file(filename::String)
