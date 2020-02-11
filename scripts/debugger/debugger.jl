936d48ce81f684a98adae130a2efe2d118fca5e1
David Anthoff <anthoff@berkeley.edu>
2020-02-10 16:54:05 -0800

    Don't show vars without a name in debugger

--- scripts/debugger/debugger.jl
+++ scripts/debugger/debugger.jl
@@ -327,7 +327,7 @@ function startdebug(pipename)
                 for v in vars
                     # TODO Figure out why #self# is here in the first place
                     # For now we don't report it to the client
-                    if !startswith(string(v.name), "#")
+                    if !startswith(string(v.name), "#") && string(v.name)!=""
                         push!(vars_as_string, string(v.name, ";", typeof(v.value), ";", v.value))
                     end
                 end
