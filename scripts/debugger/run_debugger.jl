9a04112b514cd021524860af6e6c87db6ec19f20
David Anthoff <anthoff@berkeley.edu>
2020-06-23 10:09:50 -0700

    Rework crash reporting for debugger

--- scripts/debugger/run_debugger.jl
+++ scripts/debugger/run_debugger.jl
@@ -4,17 +4,4 @@ Base.push!(LOAD_PATH, joinpath(@__DIR__, "..", "packages"))
 import VSCodeDebugger
 pop!(LOAD_PATH)
 
-let
-    local pipenames = VSCodeDebugger.DebugAdapter.clean_up_ARGS_in_launch_mode()
-    try
-        @debug "Trying to connect to debug adapter."
-        socket = VSCodeDebugger.Sockets.connect(pipenames[1])
-        try
-            VSCodeDebugger.DebugAdapter.startdebug(socket)
-        finally
-            close(socket)
-        end
-    catch err
-        VSCodeDebugger.DebugAdapter.global_err_handler(err, catch_backtrace(), pipenames[2], "Debugger")
-    end
-end
+VSCodeDebugger.startdebugger()
--- scripts/packages/VSCodeDebugger/src/VSCodeDebugger.jl
+++ scripts/packages/VSCodeDebugger/src/VSCodeDebugger.jl
@@ -1,5 +1,9 @@
 module VSCodeDebugger
 
+import Sockets
+
+include("../../../error_handler.jl")
+
 include("../../CodeTracking/src/CodeTracking.jl")
 include("../../JSON/src/JSON.jl")
 
@@ -23,6 +27,21 @@ module DebugAdapter
     import ..JSONRPC: @dict_readable, Outbound
 
     include("../../DebugAdapter2/src/packagedef.jl")
+end
+
+function startdebugger()
+    pipenames = DebugAdapter.clean_up_ARGS_in_launch_mode()
+    try
+        @debug "Trying to connect to debug adapter."
+        socket = Sockets.connect(pipenames[1])
+        try
+            DebugAdapter.startdebug(socket, (err, bt)->global_err_handler(err, bt, pipenames[2], "Debugger"))
+        finally
+            close(socket)
+        end
+    catch err
+        global_err_handler(err, catch_backtrace(), pipenames[2], "Debugger")
+    end
 end
 
 end
