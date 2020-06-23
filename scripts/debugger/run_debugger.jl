239021c0e42256614c7f1b62d3ded713d889ba90
David Anthoff <anthoff@berkeley.edu>
2020-06-23 09:49:54 -0700

    Move socket opening out of DebugAdapter

--- scripts/debugger/run_debugger.jl
+++ scripts/debugger/run_debugger.jl
@@ -7,7 +7,13 @@ pop!(LOAD_PATH)
 let
     local pipenames = VSCodeDebugger.DebugAdapter.clean_up_ARGS_in_launch_mode()
     try
-        VSCodeDebugger.DebugAdapter.startdebug(pipenames[1])
+        @debug "Trying to connect to debug adapter."
+        socket = VSCodeDebugger.Sockets.connect(pipenames[1])
+        try
+            VSCodeDebugger.DebugAdapter.startdebug(socket)
+        finally
+            close(socket)
+        end
     catch err
         VSCodeDebugger.DebugAdapter.global_err_handler(err, catch_backtrace(), pipenames[2], "Debugger")
     end
--- scripts/packages/VSCodeServer/src/debugger.jl
+++ scripts/packages/VSCodeServer/src/debugger.jl
@@ -2,7 +2,13 @@ function repl_startdebugger_request(conn, params::String, crashreporting_pipenam
     hideprompt() do
         debug_pipename = params
         try
-            DebugAdapter.startdebug(debug_pipename)
+            @debug "Trying to connect to debug adapter."
+            socket = Sockets.connect(debug_pipename)
+            try
+                DebugAdapter.startdebug(socket)
+            finally
+                close(socket)
+            end
         catch err
             DebugAdapter.global_err_handler(err, catch_backtrace(), crashreporting_pipename, "Debugger")
         end
