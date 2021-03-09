04c64b03cd37bd97aadb3b253697f852548ec979
Sebastian Pfitzner <pfitzseb@gmail.com>
2021-03-09 12:04:46 +0100

    fix debugger commands

--- src/debugger/debugFeature.ts
+++ src/debugger/debugFeature.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode'
 import * as jlpkgenv from '../jlpkgenv'
 import { getJuliaExePath } from '../juliaexepath'
-import { registerCommand } from '../utils'
 import { JuliaDebugSession } from './juliaDebug'
 
 export class JuliaDebugFeature {
@@ -12,11 +11,10 @@ export class JuliaDebugFeature {
         this.context.subscriptions.push(
             vscode.debug.registerDebugConfigurationProvider('julia', provider),
             vscode.debug.registerDebugAdapterDescriptorFactory('julia', factory),
-            registerCommand('language-julia.debug.getActiveJuliaEnvironment', async config => {
-                const pkgenvpath = await jlpkgenv.getAbsEnvPath()
-                return pkgenvpath
+            vscode.commands.registerCommand('language-julia.debug.getActiveJuliaEnvironment', async config => {
+                return await jlpkgenv.getAbsEnvPath()
             }),
-            registerCommand('language-julia.runEditorContents', async (resource: vscode.Uri | undefined) => {
+            vscode.commands.registerCommand('language-julia.runEditorContents', async (resource: vscode.Uri | undefined) => {
                 resource = getActiveUri(resource)
                 if (!resource) {
                     vscode.window.showInformationMessage('No active editor found.')
@@ -38,7 +36,7 @@ export class JuliaDebugFeature {
                     vscode.window.showErrorMessage('Could not run editor content in new process.')
                 }
             }),
-            registerCommand('language-julia.debugEditorContents', async (resource: vscode.Uri | undefined) => {
+            vscode.commands.registerCommand('language-julia.debugEditorContents', async (resource: vscode.Uri | undefined) => {
                 resource = getActiveUri(resource)
                 if (!resource) {
                     vscode.window.showInformationMessage('No active editor found.')
