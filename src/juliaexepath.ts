0b28d5dc9c752ab9642dece09b6fadf9d0336e47
David Anthoff <anthoff@berkeley.edu>
2021-07-14 17:12:13 -0700

    Add support for Julia 1.6.2

--- src/juliaexepath.ts
+++ src/juliaexepath.ts
@@ -37,6 +37,7 @@ export async function getJuliaExePath() {
             let pathsToSearch = []
             if (process.platform === 'win32') {
                 pathsToSearch = ['julia.exe',
+                    path.join(homedir, 'AppData', 'Local', 'Programs', 'Julia-1.6.2', 'bin', 'julia.exe'),
                     path.join(homedir, 'AppData', 'Local', 'Programs', 'Julia-1.6.1', 'bin', 'julia.exe'),
                     path.join(homedir, 'AppData', 'Local', 'Programs', 'Julia-1.6.0', 'bin', 'julia.exe'),
                     path.join(homedir, 'AppData', 'Local', 'Programs', 'Julia 1.5.4', 'bin', 'julia.exe'),
