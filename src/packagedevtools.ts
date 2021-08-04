85fb17ee42d86edf9e9dc17a9ed8b5b6f9acfff9
David Anthoff <anthoff@berkeley.edu>
2021-08-03 19:18:41 -0700

    Fix some strict typing bugs

--- src/packagedevtools.ts
+++ src/packagedevtools.ts
@@ -12,38 +12,41 @@ export class JuliaPackageDevFeature {
     private async tagNewPackageVersion() {
         telemetry.traceEvent('command-tagnewpackageversion')
 
-        let resultVersion = await vscode.window.showQuickPick(['Next', 'Major', 'Minor', 'Patch', 'Custom'], { placeHolder: 'Please select the version to be tagged.' })
+        if (vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0) {
 
-        if (resultVersion === 'Custom') {
-            resultVersion = await vscode.window.showInputBox({ prompt: 'Please enter the version number you want to tag.' })
-        }
+            let resultVersion = await vscode.window.showQuickPick(['Next', 'Major', 'Minor', 'Patch', 'Custom'], { placeHolder: 'Please select the version to be tagged.' })
 
-        if (resultVersion !== undefined) {
-            const bar = await vscode.authentication.getSession('github', ['repo'], { createIfNone: true })
-            const accessToken = bar.accessToken
-            const account = bar.account.label
+            if (resultVersion === 'Custom') {
+                resultVersion = await vscode.window.showInputBox({ prompt: 'Please enter the version number you want to tag.' })
+            }
 
-            const exepath = await getJuliaExePath()
+            if (resultVersion !== undefined) {
+                const bar = await vscode.authentication.getSession('github', ['repo'], { createIfNone: true })
+                const accessToken = bar.accessToken
+                const account = bar.account.label
 
-            const newTerm = vscode.window.createTerminal(
-                {
-                    name: 'Julia: Tag a new package version',
-                    shellPath: exepath,
-                    shellArgs: [
-                        path.join(this.context.extensionPath, 'scripts', 'packagedev', 'tagnewpackageversion.jl'),
-                        accessToken,
-                        account,
-                        resultVersion
-                    ],
-                    cwd: vscode.workspace.workspaceFolders[0].uri.fsPath,
-                    env: {
-                        JULIA_PROJECT: path.join(this.context.extensionPath, 'scripts', 'environments', 'pkgdev')
-                    },
+                const exepath = await getJuliaExePath()
 
-                }
-            )
+                const newTerm = vscode.window.createTerminal(
+                    {
+                        name: 'Julia: Tag a new package version',
+                        shellPath: exepath,
+                        shellArgs: [
+                            path.join(this.context.extensionPath, 'scripts', 'packagedev', 'tagnewpackageversion.jl'),
+                            accessToken,
+                            account,
+                            resultVersion
+                        ],
+                        cwd: vscode.workspace.workspaceFolders[0].uri.fsPath,
+                        env: {
+                            JULIA_PROJECT: path.join(this.context.extensionPath, 'scripts', 'environments', 'pkgdev')
+                        },
 
-            newTerm.show(true)
+                    }
+                )
+
+                newTerm.show(true)
+            }
         }
     }
 
