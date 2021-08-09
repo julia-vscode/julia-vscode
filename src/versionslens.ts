048ad8f95ca73739396f8382116550676efb6e77
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-08-09 20:03:06 +0200

    Refactor: replaced the class with a namspace and fix typing holes.
    - The only job of the class was to provide encapsulation a namespace can
      do the job.
    - Fix some typing holes in `dep`.

--- src/versionslens.ts
+++ src/versionslens.ts
@@ -4,40 +4,53 @@ import * as vscode from 'vscode'
 
 
 export function activate(context: vscode.ExtensionContext) {
-    return new VersionLensProvider(context)
+    VersionLens.register(context)
 }
 
+namespace VersionLens {
+    const UUID_LENGTH = 36
 
-class VersionLensProvider {
-    constructor(private context: vscode.ExtensionContext) {
-        this.context.subscriptions.push(vscode.languages.registerCodeLensProvider(
+    type uuid = string
+    type TomlDependencies = { [packageName: string]: uuid }
+    type ProjectToml = {
+        authors?: string[];
+        compat?: TomlDependencies;
+        deps?: TomlDependencies;
+        extras?: TomlDependencies;
+        name: string;
+        targets?: object;
+        uuid?: uuid;
+        version?: string;
+    }
+
+    export function register(context: vscode.ExtensionContext) {
+        context.subscriptions.push(vscode.languages.registerCodeLensProvider(
             { pattern: '**/Project.toml', language: 'toml' },
-            { provideCodeLenses: VersionLensProvider.provideCodeLenses },
+            { provideCodeLenses: provideCodeLenses },
         ))
     }
 
-    static provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken) {
-        const deps = VersionLensProvider.parseProjectTomlDocument(document)
-        const ranges = VersionLensProvider.getPositions(document, deps)
+    function provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken) {
+        const deps = parseProjectTomlDocument(document)
+        const ranges = getPositions(document, deps)
         return ranges.map(range =>
             new vscode.CodeLens(range, { title: 'It works', command: '', tooltip: 'It works' })
         )
     }
 
-    static parseProjectTomlDocument(document: vscode.TextDocument) {
+    function parseProjectTomlDocument(document: vscode.TextDocument) {
         const documentText = document.getText()
-        const { deps } = toml.parse(documentText)
+        const { deps } = toml.parse(documentText) as ProjectToml
         return deps
     }
 
-    static getPositions(document: vscode.TextDocument, deps) {
+    function getPositions(document: vscode.TextDocument, deps: TomlDependencies) {
         const documentText = document.getText()
 
-        const UUIDs: Array<string> = Object.values(deps)
-        const UUIDLength = 36
+        const UUIDs = Object.values(deps)
         return UUIDs.map(UUID => {
             const startPosition = documentText.indexOf(UUID)
-            const lastPosition = startPosition + UUIDLength
+            const lastPosition = startPosition + UUID_LENGTH
 
             return new vscode.Range(
                 document.positionAt(startPosition),
