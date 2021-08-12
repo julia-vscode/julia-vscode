f012286ccc5bc2aa496e5ceae246156f896f4e74
AbdulrhmnGhanem <abdoghanem160@gmail.com>
2021-08-12 23:59:01 +0200

    Add dedent function
    - Similar to python dedent.
    - It's used to make formatting `MarkdownString`s easier.
    - Copied from
      [here](https://stackoverflow.com/questions/25924057/multiline-strings-that-dont-break-indentation)
      :)

--- src/versionslens.ts
+++ src/versionslens.ts
@@ -231,3 +231,32 @@ namespace VersionLens {
         )
     }
 }
+
+function dedent(callSite, ...args) {
+    function format(str) {
+        let size = -1
+
+        return str.replace(/\n(\s+)/g, (m, m1) => {
+
+            if (size < 0)
+            {size = m1.replace(/\t/g, '    ').length}
+
+            return '\n' + m1.slice(Math.min(m1.length, size))
+        })
+    }
+
+    if (typeof callSite === 'string') {
+        return format(callSite)
+    }
+
+    if (typeof callSite === 'function') {
+        return (...args) => format(callSite(...args))
+    }
+
+    const output = callSite
+        .slice(0, args.length + 1)
+        .map((text, i) => (i === 0 ? '' : args[i - 1]) + text)
+        .join('')
+
+    return format(output)
+}
