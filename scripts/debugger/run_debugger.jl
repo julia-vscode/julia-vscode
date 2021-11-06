7a41c8be6c95574090950b96605e8f1d1a8b3c16
Sebastian Pfitzner <pfitzseb@gmail.com>
2021-11-06 19:39:14 +0100

    slightly improve debugger messaging

--- scripts/debugger/run_debugger.jl
+++ scripts/debugger/run_debugger.jl
@@ -1,11 +1,10 @@
 # ENV["JULIA_DEBUG"] = "all"
+print("> Connecting to debugger... ")
 
 pushfirst!(LOAD_PATH, joinpath(@__DIR__, "..", "packages"))
 import VSCodeDebugger
 popfirst!(LOAD_PATH)
 
 Base.load_julia_startup()
-
-printstyled("> Debugging...\n\n", bold = true)
 
 VSCodeDebugger.startdebugger()
