bb39d0429db0bb9b60a8b2fd1372573d6ebf5268
David Anthoff <anthoff@berkeley.edu>
2022-03-20 09:40:13 -0700

    Fix lint error

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 'use strict'
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { SeverityLevel } from 'applicationinsights/out/Declarations/Contracts'
 import * as fs from 'async-file'
 import { unwatchFile, watchFile } from 'async-file'
 import * as net from 'net'
