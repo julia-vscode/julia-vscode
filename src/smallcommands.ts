c3612ea9812101589875cb323b970747ab406754
Sebastian Pfitzner <pfitzseb@gmail.com>
2022-04-26 11:35:25 +0200

    Small fixes

--- src/smallcommands.ts
+++ src/smallcommands.ts
@@ -46,9 +46,10 @@ async function newJuliaFile(uri?: vscode.Uri) {
     if (uri) {
         const stat = await vscode.workspace.fs.stat(uri)
         const dir = stat.type === vscode.FileType.Directory ? uri.fsPath : path.dirname(uri.fsPath)
+        const defaultName = path.join(dir, 'untitled.jl')
         const givenPath = await vscode.window.showInputBox({
-            value: dir,
-            valueSelection: [dir.length, dir.length], // no selection
+            value: defaultName,
+            valueSelection: [dir.length + 1, defaultName.length - 3], // select file name
             prompt: 'Enter a file path to be created',
             validateInput: async input => {
                 const givenPath = vscode.Uri.file(input).fsPath
@@ -64,13 +65,9 @@ async function newJuliaFile(uri?: vscode.Uri) {
         const targetUri = vscode.Uri.file(givenPath)
         try {
             await fs.writeTextFile(targetUri.fsPath, '')
-            vscode.workspace.openTextDocument(targetUri)
-                .then(document => {
-                    return vscode.languages.setTextDocumentLanguage(document, 'julia')
-                })
-                .then(document => {
-                    vscode.window.showTextDocument(document)
-                })
+            const document = await vscode.workspace.openTextDocument(targetUri)
+            await vscode.languages.setTextDocumentLanguage(document, 'julia')
+            await vscode.window.showTextDocument(document)
         } catch (err) {
             vscode.window.showErrorMessage(`Failed to create ${targetUri.fsPath}`)
         }
@@ -79,14 +76,14 @@ async function newJuliaFile(uri?: vscode.Uri) {
         const document = await vscode.workspace.openTextDocument({
             language: 'julia'
         })
-        vscode.window.showTextDocument(document)
+        await vscode.window.showTextDocument(document)
     }
 }
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
-        vscode.commands.registerCommand('language-julia.applytextedit', applyTextEdit),
-        vscode.commands.registerCommand('language-julia.toggleLinter', toggleLinter),
-        vscode.commands.registerCommand('language-julia.newJuliaFile', newJuliaFile)
+        registerCommand('language-julia.applytextedit', applyTextEdit),
+        registerCommand('language-julia.toggleLinter', toggleLinter),
+        registerCommand('language-julia.newJuliaFile', newJuliaFile)
     )
 }
