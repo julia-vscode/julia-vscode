28e5b5088bba185bea30d5f2b8fe3ab19ed7649c
David Anthoff <anthoff@berkeley.edu>
2024-07-19 11:32:46 +0200

    Update deps

--- src/scripts/updateDeps.ts
+++ src/scripts/updateDeps.ts
@@ -46,7 +46,7 @@ async function main() {
     await our_download('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/solid.min.css', 'libs/fontawesome/solid.min.css')
     await our_download('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/brands.min.css', 'libs/fontawesome/brands.min.css')
 
-    for (const pkg of ['JSONRPC', 'CSTParser', 'LanguageServer', 'StaticLint', 'SymbolServer', 'DebugAdapter']) {
+    for (const pkg of ['JSONRPC', 'CSTParser', 'LanguageServer', 'StaticLint', 'SymbolServer', 'DebugAdapter', 'Salsa']) {
         console.log(`Updating ${pkg} to latest master`)
         await cp.exec('git checkout master', { cwd: path.join(process.cwd(), `scripts/packages/${pkg}`) })
         await cp.exec('git pull', { cwd: path.join(process.cwd(), `scripts/packages/${pkg}`) })
@@ -63,7 +63,6 @@ async function main() {
         'AutoHashEquals',
         'ExceptionUnwrapping',
         'MacroTools',
-        'Salsa',
         'CodeTracking',
         'CoverageTools',
         'FilePathsBase',
