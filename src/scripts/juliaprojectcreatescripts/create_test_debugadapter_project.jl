25e20146d3f7ddd5b438de411eb07f18f3bec20a
David Anthoff <anthoff@berkeley.edu>
2024-12-19 14:09:09 -0800

    Handle JuliaInterpreter and LoweredCodeUtils version split

--- src/scripts/juliaprojectcreatescripts/create_test_debugadapter_project.jl
+++ src/scripts/juliaprojectcreatescripts/create_test_debugadapter_project.jl
@@ -1,9 +1,17 @@
 using Pkg
 
+julia_interpreter_path = if VERSION >= v"1.10.0"
+    "../../../packages/JuliaInterpreter"
+elseif VERSION >= v"1.6.0"
+    "../../../packages-old/v1.9/JuliaInterpreter"
+else
+    "../../../packages-old/v1.5/JuliaInterpreter"
+end
+
 Pkg.develop([
     PackageSpec(path="../../../packages/CodeTracking"),
     PackageSpec(path="../../../packages/DebugAdapter"),
     PackageSpec(path="../../../packages/JSON"),
     PackageSpec(path="../../../packages/JSONRPC"),
-    VERSION >= v"v1.6.0" ? PackageSpec(path="../../../packages/JuliaInterpreter") : PackageSpec(path="../../../packages-old/v1.5/JuliaInterpreter"),
+    PackageSpec(path=julia_interpreter_path),
 ])
