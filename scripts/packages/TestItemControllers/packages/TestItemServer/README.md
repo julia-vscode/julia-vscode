9e64bbc5e557f01ed3d0819cd6df5a5064cddbf9
David Anthoff <anthoff@berkeley.edu>
2024-12-27 22:29:46 -0800

    Remove old test item code

--- scripts/packages/TestItemControllers/packages/TestItemServer/README.md
+++
@@ -1,3 +0,0 @@
-# TestItemServer.jl
-
-This package implements the functionality that runs inside a test item test process. The package is not registered, but instead vendored by both the VS Code extension and the TestItemRunner.jl package.
--- src/scripts/updateDeps.ts
+++ src/scripts/updateDeps.ts
@@ -52,7 +52,7 @@ async function main() {
         await cp.exec('git pull', { cwd: path.join(process.cwd(), `scripts/packages/${pkg}`) })
     }
 
-    for (const pkg of ['IJuliaCore', 'JuliaWorkspaces', 'TestItemServer', 'TestItemDetection']) {
+    for (const pkg of ['IJuliaCore', 'JuliaWorkspaces', 'TestItemControllers', 'TestItemDetection']) {
         console.log(`Updating ${pkg} to latest main`)
         await cp.exec('git checkout main', { cwd: path.join(process.cwd(), `scripts/packages/${pkg}`) })
         await cp.exec('git pull', { cwd: path.join(process.cwd(), `scripts/packages/${pkg}`) })
