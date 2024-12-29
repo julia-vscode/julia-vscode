121cbd9101d866ccbd6d3901ebb03da5465e92ef
David Anthoff <anthoff@berkeley.edu>
2024-12-28 16:16:28 -0800

    Update test item controller env creation script

--- src/scripts/juliaprojectcreatescripts/create_testitemcontroller_project.jl
+++ src/scripts/juliaprojectcreatescripts/create_testitemcontroller_project.jl
@@ -1,9 +1,3 @@
 using Pkg
 
-Pkg.add("Pkg")
-Pkg.develop(PackageSpec(path="../../../packages/AutoHashEquals"),)
-Pkg.develop(PackageSpec(path="../../../packages/CoverageTools"),)
-Pkg.develop(PackageSpec(path="../../../packages/JSON"),)
-Pkg.develop(PackageSpec(path="../../../packages/JSONRPC"),)
 Pkg.develop(PackageSpec(path="../../../packages/TestItemControllers"),)
-Pkg.develop(PackageSpec(path="../../../packages/URIParser"),)
