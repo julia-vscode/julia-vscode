632b467f074bfb288e3bbe546098023d0ba783db
pfitzseb <pfitzseb@gmail.com>
2025-11-17 17:19:39 +0100

    feat: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,18 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
-export default [
+export default defineConfig(
     {
-        ignores: ['out/', 'libs/', 'dist/', '.vscode-test/']
+        ignores: ['out/', 'libs/', 'dist/', '.vscode-test/', 'scripts/'],
     },
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
     {
         files: ['**/*.ts'],
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-        },
-
-        languageOptions: {
-            parser: tsParser,
-            ecmaVersion: 2022,
-            sourceType: 'module',
-        },
-
         rules: {
-            '@typescript-eslint/naming-convention': ['warn', {
-                selector: 'import',
-                format: ['camelCase', 'PascalCase'],
-            }],
-
             curly: 'warn',
             eqeqeq: 'error',
             'no-throw-literal': 'warn',
@@ -31,4 +21,4 @@ export default [
             'no-var': 'warn',
         },
     }
-]
+)
