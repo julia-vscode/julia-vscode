4ddd8fe963f56d0d2d608d3a527fdaff4fb209b1
David Anthoff <anthoff@berkeley.edu>
2026-04-03 16:24:17 -0700

    Reenable type checking and update npm structure

---
+++ esbuild.js
@@ -0,0 +1,57 @@
+const esbuild = require('esbuild');
+
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+    name: 'esbuild-problem-matcher',
+
+    setup(build) {
+        build.onStart(() => {
+            console.log('[watch] build started');
+        });
+        build.onEnd((result) => {
+            result.errors.forEach(({ text, location }) => {
+                console.error(`✘ [ERROR] ${text}`);
+                if (location == null) return;
+                console.error(`    ${location.file}:${location.line}:${location.column}:`);
+            });
+            console.log('[watch] build finished');
+        });
+    },
+};
+
+async function main() {
+    const ctx = await esbuild.context({
+        entryPoints: ['src/extension.ts'],
+        bundle: true,
+        format: 'cjs',
+        minify: production,
+        sourcemap: true,
+        sourcesContent: false,
+        platform: 'node',
+        outdir: './dist',
+        external: [
+            'vscode',
+            'applicationinsights-native-metrics',
+            '@opentelemetry/instrumentation',
+            '@azure/opentelemetry-instrumentation-azure-sdk',
+        ],
+        logLevel: 'warning',
+        plugins: [esbuildProblemMatcherPlugin],
+    });
+    if (watch) {
+        await ctx.watch();
+    } else {
+        await ctx.rebuild();
+        await ctx.dispose();
+    }
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
