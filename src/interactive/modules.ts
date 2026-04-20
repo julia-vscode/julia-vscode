4b400f681bd941982e10ce636e10264fb2c69f1f
David Anthoff <anthoff@berkeley.edu>
2026-04-20 14:05:41 +0200

    Fix a lint error

--- src/interactive/modules.ts
+++ src/interactive/modules.ts
@@ -124,7 +124,7 @@ export async function getModuleForEditor(
                 throw err
             }
         },
-        (err) => {
+        (_err) => {
             return { module: 'Main' }
         }
     )
